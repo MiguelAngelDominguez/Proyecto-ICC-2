e4fa406856a2a4fe7b6cdb73a51c6ca1e9976f41
MatCavUTEC <mathias.cavalcanti@utec.edu.pe>
2025-11-25 22:48:48 -0500

    add change

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from math import sqrt
 import cv2 as cv
+import statistics
 
 ORIGINAL_ESC = 255
 DESTINATION_ESC = 16
@@ -14,11 +15,10 @@ IDENTIFICARDOR DE NUMEROS POR DNI
  - Ingrese su DNI -> """
 
 dict_number_test = {
-
+    0:"cero", 1:"uno", 2:"dos", 3:".tres", 4:"cuatro", 5:"cinco", 6:"seis", 7:"siete", 8:"ocho", 9:"nueve"
 }
 
 datos_number_dataset = datasets.load_digits()
-print(dir(datos_number_dataset))
 
 def euclidean_distance_matrix(A: np.ndarray, B: np.ndarray) -> float:
     assert A.shape == (8,8) and B.shape == (8,8)
@@ -46,12 +46,10 @@ def process_input_image(img: str) -> list[list]:
         i += 1
     return scale8_img
 
-def approach_pass(array, list_number_aprox: list) -> None:
+def approach_pass(array, list_number_aprox: list):
     for i in range(len(datos_number_dataset["images"])):
         d = normalized_distance(array, datos_number_dataset["images"][i])
-        if d < 0.30:
-            print(d)
-            list_number_aprox.append((round(d, 8), i))
+        list_number_aprox.append((round(d, 8), i))
 
 def detection_numbers(img: str, list_number_aprox: list) -> list:
     array = process_input_image(img)
@@ -59,18 +57,26 @@ def detection_numbers(img: str, list_number_aprox: list) -> list:
     list_number_aprox.sort()
     return list_number_aprox
 
-def performance_test(img: str) -> int:
+def performance_test(img: str, numero: int):
+    print(f"El número a evaluar es: {numero}\n")
+
     list_number_aprox = []
     list_number_aprox = detection_numbers(img, list_number_aprox)
-
+    three_digits = []
     print(list_number_aprox)
+    print("Los targets de los 3 vecinos más cercanos son: ")
     for i in range(3):
-        print(datos_number_dataset["images"][list_number_aprox[i][1]])
-        print("El numero se aproxima a ->", datos_number_dataset["target"][list_number_aprox[i][1]])
-        print("___________________________")
+        three_digits.append(datos_number_dataset["target"][list_number_aprox[i][1]])
+        print(datos_number_dataset["target"][list_number_aprox[i][1]], end=" ")
+    print("\n")
+    print("La etiqueta verdadera es: ")
+    list_targets = []
+    for i in range(len(list_number_aprox[:100])):
+        list_targets.append(datos_number_dataset["target"][list_number_aprox[i][1]])
+    moda = statistics.mode(list_targets)
+    print(f"{moda}\n")
 
-url_img = "./test/img/nueve_test.png"
-performance_test(url_img)
+    print(f"Soy la inteligencia artificial, y he detectado que el dígito ingresado corresponde al número {moda}")
 
 def menu():
     while True:
@@ -86,11 +92,12 @@ def menu():
             print(e)
             print("Se ingreso de manera incorrecta el DNI")
             continue
-        list_dni = [int(n) for n in str(dni)]
-        for numb in list_dni:
-            print(numb)
 
-        
+        list_dni = [int(n) for n in str(dni)]
+        for i in list_dni:
+            url_img = f"./test/img/{dict_number_test[i]}.png"
+            print(performance_test(url_img,i))
 
 if __name__ == "__main__":
     menu()
+
